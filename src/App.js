58b76e23d3a38b894bd1a3fb68990f95f4dc384e
Steven <steven.r987@gmail.com>
2021-06-28 20:00:02 -0600

    getting selections

--- src/App.js
+++ src/App.js
@@ -14,67 +14,81 @@ const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
 // separate db logic from app
 export function App() {
 
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const [selections, setSelections] = useState([]);
+  const [selections, setSelections] = useState([]);
 
 
-    //one solution is to include a query when calling load - finding the right data
-    async function load() {
-        const worker = await createDbWorker(
-          [
-            {
-              from: "inline",
-              config: {
-                serverMode: "full",
-                url: "/games.db",
-                requestChunkSize: 4096,
-              },
+  //one solution is to include a query when calling load - finding the right data
+  async function load() {
+      const worker = await createDbWorker(
+        [
+          {
+            from: "inline",
+            config: {
+              serverMode: "full",
+              url: "/games.db",
+              requestChunkSize: 4096,
             },
-          ],
-          workerUrl.toString(),
-          wasmUrl.toString()
-        );
-      
-        return await worker.db.query(`select * from gamesToRecommend`);
+          },
+        ],
+        workerUrl.toString(),
+        wasmUrl.toString()
+      );
+    
+      return await worker.db.query(`select * from gamesToRecommend`);
+  }
+
+  const retrieveData = async() => {
+      try {
+          var response = await load();
+          setData(response);
+      } catch(error){
+          console.log(error);
+      }
+  };
+
+  useEffect(() => {
+      retrieveData();
+  }, []);
+
+  useEffect(()=> {
+    setSelections();
+  }, [selections]);
+
+
+  //getting which pics were selected
+  const getSelections = (pics) => {
+    setSelections(pics)
+  }
+
+  //recommends second row of games
+  //so probably just get keywords eg genre and rating and then preform a search using those filters
+
+  //create a sql query based on recieved values
+  const recommend = () => {
+    //multiple genres can come in, so we need to identify all possible genre matches then proceed
+    var selectedGames = [];
+    const values = Object.values(selections)
+    const input = values[0];
+
+    console.log(input);
+    
+    for (var imgInfo in input){
+      console.log(imgInfo);
     }
 
-    const retrieveData = async() => {
-        try {
-            var response = await load();
-            setData(response);
-        } catch(error){
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        retrieveData();
-    }, []);
-
-    useEffect(()=> {
-      setSelections();
-    }, [selections]);
-
-
-    //passing data
-    const getSelections = (pics) => {
-      setSelections(pics)
-    }
-
-    //recommends second row of games
-    const recommend = () => {
-
-    }
+    return (`select * from gamesToRecommend`)
+  }
 
     
-    return (
-        
-        <div className="app">
-             {data.length ? <ImageContainer data={data} getSelections={getSelections}/> : <h1>Loading...</h1>}
-             {selections ? console.log(selections) : "No selections yet"}
-        </div>
-    );
+  return (
+      
+      <div className="app">
+            {data.length ? <ImageContainer data={data} getSelections={getSelections}/> : <h1>Loading...</h1>}
+            {selections ? recommend() : null}
+      </div>
+  );
 }
 
 export default App;
--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -1,34 +1,24 @@
 import React, {useState} from 'react'
 import ImagePicker from 'react-image-picker'
-//const images = require.context('../public/images', true);
 
 const ImageContainer = ({ data, getSelections }) => {
-    const [images, setImage] = useState([]);
-    var games = [];
 
-    
-    //Creates an array containing image urls
-    //Could limit display by changing length to a fixed number eg 5
-    for (var j = 0; j < 5; j++){
-        games[j] = './public/images/' + data[j].img;
-    }
+    const [images, setImage] = useState([]);
 
     const onPick = (images) => {
       setImage({images});
     }
 
-
     return (
         <div>
           <ImagePicker 
-            images={games.map((image, i) => ({src: image, value: i}))}
+            images={data.map((values) => ({src: ('./public/images/' + values.img), value: values.pk}))}
             onPick={onPick}
             multiple
           />
           <button type="button" onClick={() => getSelections(images)}>Submit</button>
         </div>
     )
-    
 };
 
 export default ImageContainer;
