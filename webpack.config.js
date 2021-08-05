d866c11454f8c0101e04d7eda8d7429c0d9b21ad
Steven <steven.r987@gmail.com>
2021-08-05 16:54:41 -0600

    github pages updates

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/docs'),
         filename: 'index.bundle.js',
     },
     devServer: {
