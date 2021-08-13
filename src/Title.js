781d6c2f6e59108d8efa9e141eef8b47accabb62
Steven <steven.r987@gmail.com>
2021-08-13 12:50:03 -0600

    UI positioning

--- src/Title.js
+++ src/Title.js
@@ -8,7 +8,7 @@ const Title = (props) => {
         <div className="title">
             <Info color="white" size={32} className="shuffle" onClick={info}/>
             <Shuffle color="white" size={32} className="shuffle" onClick={refreshPage}/>
-            <h1>Game Recommender v2</h1>
+            <h1>Game Recommender</h1>
         </div>
     )
 }
